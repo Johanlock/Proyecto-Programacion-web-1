0b2b834573f388c491ea83fad4d5fbfcc383c5ff
Johanlocker <67562666+Johanlock@users.noreply.github.com>
2020-10-17 17:57:39 -0400

    All project

---
+++ Proyecto RRHH(Servicios)/Services/Servicios_Empleados.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Proyecto_RRHH__Datos_.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Proyecto_RRHH_Servicios_.Services
+{
+    public class Servicios_Empleados
+    {
+        ProjectRRHHContext DB = new ProjectRRHHContext();
+
+        public void Nuevo(Empleados id)
+        {
+            id.CodigoEmpleador = NuevoCodigo();
+            DB.Empleados.Add(id);
+            DB.SaveChanges();
+        }
+
+        public List<Empleados> ConsultaGeneral()
+        {
+            return DB.Empleados.Include(x => x.PuestoNavigation).Include(x => x.EstadoNavigation).ToList();
+        }
+
+        public Empleados ConsultaPorCodigo(int id)
+        {
+            return DB.Empleados.Include(x=>x.PuestoNavigation).Include(x => x.EstadoNavigation).FirstOrDefault(x=>x.CodigoEmpleador == id);
+        }
+
+        public int NuevoCodigo()
+        {
+            if(DB.Empleados.Select(x=>x).Count() >= 1)
+            {
+                return DB.Empleados.OrderByDescending(x => x.CodigoEmpleador).FirstOrDefault().CodigoEmpleador + 1;
+            }
+            return 1;
+            
+        }
+    }
+}
---
+++ Proyecto RRHH/Controllers/EmpleadosController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Proyecto_RRHH_Servicios_.Services;
+
+namespace Proyecto_RRHH.Controllers
+{
+    public class EmpleadosController : Controller
+    {
+        Servicios_Empleados sv = new Servicios_Empleados();
+
+        public IActionResult Index()
+        {
+            var Model = sv.ConsultaGeneral();
+            return View(Model);
+        }
+        public IActionResult Details(int id)
+        {
+            var Model = sv.ConsultaPorCodigo(id);
+            return View(Model);
+        }
+    }
+}
